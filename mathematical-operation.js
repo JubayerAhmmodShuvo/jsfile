080eb24a111a9d4e726edb0c682c8d69d1850f82
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-01-26 00:17:52 +0600

    mathematical operations

---
+++ mathematical-operation.js
@@ -0,0 +1,16 @@
+var onionPrice = 43;
+var eggPrice = 42;
+var eggQuantity = 7;
+var totalPrice = onionPrice + eggPrice;
+console.log(onionPrice);
+console.log(eggPrice);
+console.log(onionPrice + eggPrice);
+console.log(totalPrice);
+var totalDifference = onionPrice - eggPrice;
+console.log(totalDifference);
+var eggPriceMultiplication = eggPrice * eggQuantity;
+
+var moneyInPocket = 100;
+var orangePrice = 5;
+var orangeQuantity = moneyInPocket / orangePrice;
+console.log(orangeQuantity);
