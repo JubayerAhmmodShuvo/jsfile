8a0f748c6e301bc945c2583218d291bce43ce218
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-01-25 23:48:25 +0600

    variable practices

--- variable.js
+++ variable.js
@@ -1,3 +1,11 @@
 var price = 10;
 var myage = 23;
 var temperature = 37;
+
+var name = "sadar uddin";
+var schoolname = "sadar uddin school";
+var address = "sadar uddin school, dhaka";
+
+var ispass = true;
+var isfail = false;
+var subscribe = false;
