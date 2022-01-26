359ec4a5c9bbffdcef052fceadee5bdb64f77aeb
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-01-26 11:42:56 +0600

    practice

--- mathematical-operation.js
+++ mathematical-operation.js
@@ -9,8 +9,20 @@ console.log(totalPrice);
 var totalDifference = onionPrice - eggPrice;
 console.log(totalDifference);
 var eggPriceMultiplication = eggPrice * eggQuantity;
-
 var moneyInPocket = 100;
 var orangePrice = 5;
 var orangeQuantity = moneyInPocket / orangePrice;
 console.log(orangeQuantity);
+
+var date = new Date();
+console.log(date);
+
+var firstNumber = 0.1;
+var secondNumber = 0.2;
+var sum = firstNumber + secondNumber;
+console.log(sum.toFixed(2));
+
+var firstName = "John";
+var lastName = "Smith";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
