ae8eb5b432db904c7f362cecd06e0cb10d9bfcaf
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-01-26 23:30:31 +0600

    tada

---
+++ js-fundamentals/arary.js
@@ -0,0 +1,9 @@
+var friendsAge = [11, 13, 17];
+
+var muriChanacurFee = [34, 55, 221, 3];
+
+var nauikar = ['mahi', 'opu', 'sabana', 'kopila'];
+
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+console.log(vowels.length);
