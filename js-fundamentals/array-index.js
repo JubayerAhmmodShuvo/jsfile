cfc1b588e3764ee94b5234e5d3fdb90c094594a3
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-01-26 23:50:43 +0600

    d

---
+++ js-fundamentals/array-index.js
@@ -0,0 +1,13 @@
+var bookPages = ['hablu', 'kablu', 'dablu', 'battery', 'peracitamol', 'dablu', 'dodo'];
+
+var batteryIndex = bookPages.indexOf('battery');
+console.log(batteryIndex);
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var index = numbers.indexOf('122');
+console.log(index);
+
+var fifthIndex = bookPages[55];
+console.log(fifthIndex);
+numbers[2] = 22;
+console.log(numbers);
