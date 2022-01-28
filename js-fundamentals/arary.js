06d642935353fead2dcf948761c392c284d230ba
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-01-28 21:56:51 +0600

    all pushed

--- js-fundamentals/arary.js
+++ js-fundamentals/arary.js
@@ -7,3 +7,6 @@ var nauikar = ['mahi', 'opu', 'sabana', 'kopila'];
 var vowels = ['a', 'e', 'i', 'o', 'u'];
 
 console.log(vowels.length);
+console.log(vowels[2]);
+var nums = [1, 5, 8];
+console.log(nums.indexOf(1));
