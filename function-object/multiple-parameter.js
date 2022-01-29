9cb6b009510f89fa155fddddaf1edbbc98f66aed
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-01-29 09:48:40 +0600

    d

---
+++ function-object/multiple-parameter.js
@@ -0,0 +1,31 @@
+function addTwoNumbers(a, b) {
+    console.log(a, b);
+    total = a + b;
+    return total;
+}
+var result = addTwoNumbers(10, 20);
+console.log("total is", result);
+
+function multiplyTwoNumbers(a, b) {
+    console.log(a, b);
+    total = a * b;
+    return total;
+}
+var result = multiplyTwoNumbers(10, 20);
+console.log("total is", result);
+
+function substract(a, b) {
+    console.log(a, b);
+    total = a - b;
+    return total;
+}
+var result = substract(10, 20);
+console.log("total is", result);
+
+function divide(a, b) {
+    console.log(a, b);
+    total = a / b;
+    return total;
+}
+var result = divide(10, 20);
+console.log("total is", result);
---
+++ function-object/parameter.js
@@ -0,0 +1,9 @@
+function bringSingara(taka) {
+    console.log("singara er jnno dise", taka);
+    var singaraPrice = 10;
+    var singaraQuantity = taka / singaraPrice; //ddddddddddddddddddddddd
+    return singaraQuantity;
+}
+
+var singara = bringSingara(100);
+console.log("ai nen singara", singara);
