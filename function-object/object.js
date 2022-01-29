c8696176b6f78a9d7835efaeaa2dba25d7fccb70
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-01-29 10:40:32 +0600

    object variable added

--- function-object/object.js
+++ function-object/object.js
@@ -1,6 +1,15 @@
 var mobile = {
-    color: "black",
-    name: "iphone",
-    price: "1000usd",
-    storage: "16gb"
-}
+        color: "black",
+        name: "iphone",
+        price: "1000usd",
+        storage: "16gb"
+    }
+    //1
+    //console.log(mobile.color);
+    //console.log(mobile);
+    //2
+mobile['color'] = "bl ";
+//3
+var colorProperty = 'color';
+mobile[colorProperty] = "red";
+console.log(mobile);
