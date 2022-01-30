b48444073c067aec5b8960320e7f1a4f4fa8e797
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-01-30 12:14:05 +0600

    leap year

---
+++ applyjs/leapyear.js
@@ -0,0 +1,12 @@
+function isLeapYear(year) {
+    if (year % 4 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+const myYear = 2087;
+const isMyYearLeapYear = isLeapYear(myYear);
+console.log('My Year', isMyYearLeapYear);
