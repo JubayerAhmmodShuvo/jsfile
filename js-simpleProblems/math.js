a1ddf41397ce15ad8e4801bc42d3401588f5e74c
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-01-31 23:09:07 +0600

    math practice

---
+++ js-simpleProblems/math.js
@@ -0,0 +1,17 @@
+// // pos neg ja e dao positive banai dibe
+// const myNumber = -5;
+// const output = Math.abs(myNumber);
+// console.log(output);
+
+const myNumber = 21.4234455;
+const output1 = Math.ceil(myNumber);
+const output2 = Math.floor(myNumber);
+const output3 = Math.round(myNumber);
+
+console.log(output1);
+console.log(output2);
+console.log(output3);
+
+const output = Math.random() * 10;
+const roundedOutput = Math.round(output)
+console.log(roundedOutput);
