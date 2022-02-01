db369a81e70ff3f1d17237c7e7dc7b81989e326d
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-02-01 14:38:06 +0600

    fibonacci added

--- js-simpleProblems/fibo.js
+++ js-simpleProblems/fibo.js
@@ -1,7 +1,25 @@
-//find fibonacci number of a given series of numbers
+/* //find fibonacci number of a given series of numbers
 
 const fibo = [0, 1];
 for (let i = 2; i <= 10; i++) {
     fibo[i] = fibo[i - 1] + fibo[i - 2];
 }
-console.log(fibo);
+console.log(fibo); */
+
+function fibonacciSeries(num) {
+    if (typeof num != 'number') {
+        return 'please give a n umber';
+    }
+    if (num < 2) {
+        return 'please enter a positive number grater than 1';
+    }
+
+
+    const fibo = [0, 1];
+    for (let i = 2; i <= num; i++) {
+        fibo[i] = fibo[i - 1] + fibo[i - 2];
+    }
+    return fibo;
+}
+const fibonacci_series = fibonacciSeries(10);
+console.log(fibonacci_series);
