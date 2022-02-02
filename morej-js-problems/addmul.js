b9f5105bbac75deb45f4cf7df9eb9fcc5feee408
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-02-02 12:52:00 +0600

    handle error

---
+++ morej-js-problems/addmul.js
@@ -0,0 +1,14 @@
+function add(num1, num2) {
+    const sum = num1 + num2;
+    return sum;
+
+}
+const firstTotal = add(1, 2);
+const secondTotal = add(4, 5);
+
+function multiply(num1, num2) {
+    const product = num1 * num2;
+    return product;
+}
+
+//handle unexpected error in functoin
