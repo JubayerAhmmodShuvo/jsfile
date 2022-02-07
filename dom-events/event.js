959a7d9910ebc0a5c7cdc7067228b4e097ee67c1
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-02-08 00:28:36 +0600

    valo

--- dom-events/event.js
+++ dom-events/event.js
@@ -10,3 +10,23 @@ bluebutton.onclick = makeBlue;
 function makeBlue() {
     document.body.style.backgroundColor = 'blue';
 }
+
+//anonimous function
+
+const greenButton = document.getElementById('meke-green');
+greenButton.onclick = function makegreen() {
+        document.body.style.backgroundColor = 'green';
+    }
+    //addEventListener
+const goldenButton = document.getElementById('makegolden');
+goldenButton.addEventListener('click', function() {
+    document.body.style.backgroundColor = 'gold';
+});
+
+//diffent version of adding addEventListener using function
+/* 
+const goldenButton = document.getElementById('makegolden');
+goldenButton.addEventListener('click', makegolden);
+function makegolden() {document.body.style.backgroundColor = 'gold';}
+
+*/
