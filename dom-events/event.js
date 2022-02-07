c7454942484544654171b62de0e4982c7fcdd1c3
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-02-08 00:08:08 +0600

    valo

---
+++ dom-events/event.js
@@ -0,0 +1,12 @@
+function makeRed() {
+    document.body.style.backgroundColor = 'red';
+}
+
+
+
+const bluebutton = document.getElementById('make-blue');
+bluebutton.onclick = makeBlue;
+
+function makeBlue() {
+    document.body.style.backgroundColor = 'blue';
+}
